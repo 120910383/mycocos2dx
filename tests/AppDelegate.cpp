bf79ed5077dd8bf765da45a89bea8d8bcfd7d474
congcong <120910383@qq.com>
2012-05-17 09:05:41 +0000

    #4 完善CCApplication应用程序类和CCEGLView窗口创建管理类，并使用这两个类新建一个测试工程来显示窗口 3.创建测试工程，依赖项为cocos2dx,类型为Win32应用程序

---
+++ tests/AppDelegate.cpp
@@ -0,0 +1,42 @@
+﻿#include "AppDelegate.h"
+#include "CCEGLView.h"
+
+AppDelegate::AppDelegate()
+{
+
+}
+
+AppDelegate::~AppDelegate()
+{
+
+}
+
+bool AppDelegate::initInstance()
+{
+	bool bRet = false;
+	do 
+	{
+		cocos2d::CCEGLView* pMainWnd = new cocos2d::CCEGLView;
+		if (!pMainWnd || !pMainWnd->Create(TEXT("mycocos2dx"), 480, 320))
+			break;
+
+		bRet = true;
+	} while (0);
+
+	return bRet;
+}
+
+bool AppDelegate::applicationDidFinishLaunching()
+{
+	return true;
+}
+
+void AppDelegate::applicationDidEnterBackground()
+{
+
+}
+
+void AppDelegate::applicationWillEnterForeground()
+{
+
+}
---
+++ tests/main.cpp
@@ -0,0 +1,15 @@
+﻿#include "main.h"
+#include "AppDelegate.h"
+
+int APIENTRY _tWinMain(HINSTANCE hInstance,
+					   HINSTANCE hPrevInstance,
+					   LPTSTR lpCmdLine,
+					   int nShowCmd )
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+	// create the application instance
+	AppDelegate app;
+	return cocos2d::CCApplication::sharedApplication().run();
+}
