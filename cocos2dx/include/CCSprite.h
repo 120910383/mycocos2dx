75dea418636a67db84f658f882c41bc811a86bdb
congcong <120910383@qq.com>
2012-08-09 06:20:05 +0000

    #11 创建精灵类CCSprite,可以读取png文件，并可用opengles显示出来  4、创建CCSprite精灵类,根据纹理索引显示贴图纹理

---
+++ cocos2dx/include/CCSprite.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include "CCNode.h"
+
+NS_CC_BEGIN;
+
+class CCTexture2D;
+class CC_DLL CCSprite : public CCNode
+{
+public:
+	virtual void draw();
+
+public:
+	static CCSprite* spriteWithFile(const char *pszFileName);
+
+public:
+	CCSprite();
+	virtual ~CCSprite();
+	virtual bool init();
+	bool initWithFile(const char *pszFilename);
+	bool initWithTexture(CCTexture2D *pTexture, const CCRect& rect);
+	bool initWithTexture(CCTexture2D *pTexture);
+	virtual void setTexture(CCTexture2D *texture);
+	virtual CCTexture2D* getTexture();
+	void setTextureRect(const CCRect& rect);
+
+protected:
+	CCTexture2D* m_pobTexture;
+};
+
+NS_CC_END;
---
+++ cocos2dx/sprite_nodes/CCSprite.cpp
@@ -0,0 +1,163 @@
+﻿#include "CCSprite.h"
+#include "CCPointExtension.h"
+#include "CCTexture2D.h"
+#include "CCFileUtils.h"
+#include "CCImage.h"
+
+NS_CC_BEGIN;
+
+void CCSprite::draw()
+{
+	CCNode::draw();
+
+	// 暂不实现颜色渲染
+	glDisableClientState(GL_COLOR_ARRAY);
+
+	if (NULL != m_pobTexture)
+	{
+		glBindTexture(GL_TEXTURE_2D, m_pobTexture->getName());
+	}
+	else
+	{
+		glBindTexture(GL_TEXTURE_2D, 0);
+	}
+	
+	GLfloat vertices[] = 
+	{
+		m_tPosition.x, m_tPosition.y + m_tContentSize.height, 0,
+		m_tPosition.x, m_tPosition.y, 0,
+		m_tPosition.x + m_tContentSize.width, m_tPosition.y + m_tContentSize.height, 0,
+		m_tPosition.x + m_tContentSize.width, m_tPosition.y, 0,
+	};
+	glVertexPointer(3, GL_FLOAT, 0, vertices);
+
+	GLfloat coordinates[] = 
+	{
+		0, 2, 
+		0, 0, 
+		2, 2, 
+		2, 0,
+	};
+	glTexCoordPointer(2, GL_FLOAT, 0, coordinates);
+	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+
+	glEnableClientState(GL_COLOR_ARRAY);
+}
+
+CCSprite* CCSprite::spriteWithFile(const char *pszFileName)
+{
+	CCSprite* pobSprite = new CCSprite();
+	if (NULL != pobSprite && pobSprite->initWithFile(pszFileName))
+	{
+		pobSprite->autorelease();
+		return pobSprite;
+	}
+	CC_SAFE_DELETE(pobSprite);
+	return NULL;
+}
+
+CCSprite::CCSprite()
+	: m_pobTexture(NULL)
+{
+
+}
+
+CCSprite::~CCSprite()
+{
+	CC_SAFE_RELEASE(m_pobTexture);
+}
+
+bool CCSprite::init()
+{
+	setTexture(NULL);
+
+	setAnchorPoint(ccp(0.5f, 0.5f));
+
+	return true;
+}
+
+bool CCSprite::initWithFile(const char *pszFilename)
+{
+	CCAssert(NULL != pszFilename, "");
+	CCTexture2D* pTexture = NULL;
+
+	// 根据文件名创建纹理对象
+	{
+		std::string fullpath = CCFileUtils::fullPathFromRelativePath(pszFilename);
+		std::string lowerCase(pszFilename);
+		for (unsigned int i = 0; i < lowerCase.length(); ++i)
+		{
+			lowerCase[i] = (char)tolower(lowerCase[i]);
+		}
+
+		do 
+		{
+			CCImage image;
+			CCFileData data(fullpath.c_str(), "rb");
+			unsigned long nSize = data.getSize();
+			unsigned char* pBuffer = data.getBuffer();
+			CC_BREAK_IF( !image.initWithImageData((void*)pBuffer, nSize, CCImage::kFmtPng));
+
+			pTexture = new CCTexture2D;
+			if (NULL != pTexture && pTexture->initWithImage(&image))
+			{
+				pTexture->autorelease();
+			}
+			else
+			{
+				CC_SAFE_DELETE(pTexture);
+			}
+		} while (0);
+	}
+
+	if (NULL != pTexture)
+	{
+		CCRect rect = CCRectZero;
+		rect.size = pTexture->getContentSize();
+		return initWithTexture(pTexture, rect);
+	}
+
+	return false;
+}
+
+bool CCSprite::initWithTexture(CCTexture2D *pTexture, const CCRect& rect)
+{
+	CCAssert(NULL != pTexture, "");
+	init();
+	setTexture(pTexture);
+	setTextureRect(rect);
+
+	return true;
+}
+
+bool CCSprite::initWithTexture(CCTexture2D *pTexture)
+{
+	CCAssert(NULL != pTexture, "");
+
+	CCRect rect = CCRectZero;
+	rect.size = pTexture->getContentSize();
+
+	return initWithTexture(pTexture, rect);
+}
+
+void CCSprite::setTexture(CCTexture2D *texture)
+{
+	CC_SAFE_RELEASE(m_pobTexture);
+	m_pobTexture = texture;
+	if (NULL != texture)
+	{
+		texture->retain();
+	}
+}
+
+CCTexture2D* CCSprite::getTexture()
+{
+	return m_pobTexture;
+}
+
+void CCSprite::setTextureRect(const CCRect& rect)
+{
+	setContentSize(rect.size);
+}
+
+NS_CC_END;
