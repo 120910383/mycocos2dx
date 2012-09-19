181ee84efc877371ee70f764fef23f83ce84276a
congcong <120910383@qq.com>
2012-09-19 10:54:11 +0000

    #15 创建CCMenu和CCMenuItem 根据基类CCMenuItem来创建具体的字符串菜单类CCMenuItemLabel、精灵菜单类CCMenuItemSprite

--- cocos2dx/include/CCMenuItem.h
+++ cocos2dx/include/CCMenuItem.h
@@ -4,6 +4,8 @@
 
 NS_CC_BEGIN;
 
+//////////////////////////////////////////////////////////////////////////
+// 基类，自定义的item均可从次继承
 class CC_DLL CCMenuItem : public CCNode
 {
 public:
@@ -30,6 +32,59 @@ protected:
 	bool m_bIsEnabled;
 	CCObject* m_pListener;
 	SEL_MenuHandler m_pfnSelector;
+};
+
+
+//////////////////////////////////////////////////////////////////////////
+// 字符串菜单，具有点击执行自动放大缩小动画的特性
+class CC_DLL CCMenuItemLabel : public CCMenuItem
+{
+public:
+	static CCMenuItemLabel* itemWithLabel(CCNode* label);
+	static CCMenuItemLabel* itemWithLabel(CCNode* label, CCObject* target, SEL_MenuHandler selector);
+
+public:
+	CCMenuItemLabel();
+	virtual ~CCMenuItemLabel();
+	bool initWithLabel(CCNode* label, CCObject* target, SEL_MenuHandler selector);
+
+	void setString(const char* label);
+
+	virtual void setLabel(CCNode* label);
+	virtual CCNode* getLabel();
+
+protected:
+	CCNode* m_pLabel;
+};
+
+
+//////////////////////////////////////////////////////////////////////////
+// 精灵菜单，可呈现三种状态精灵(正常，选中，非使能)
+class CC_DLL CCMenuItemSprite : public CCMenuItem
+{
+public:
+	static CCMenuItemSprite* itemFromNormalSprite(CCNode* normalSprite, CCNode* selectedSprite, CCNode* disabledSprite = NULL);
+	static CCMenuItemSprite* itemFromNormalSprite(CCNode* normalSprite, CCNode* selectedSprite, CCObject* target, SEL_MenuHandler selector);
+	static CCMenuItemSprite* itemFromNormalSprite(CCNode* normalSprite, CCNode* selectedSprite, CCNode* disabledSprite, CCObject* target, SEL_MenuHandler selector);
+
+public:
+	CCMenuItemSprite();
+	virtual ~CCMenuItemSprite();
+	bool initFromNormalSprite(CCNode* normalSprite, CCNode* selectedSprite, CCNode* disabledSprite, CCObject* target, SEL_MenuHandler selector);
+
+	virtual void setNormalImage(CCNode* normalSprite);
+	virtual CCNode* getNormalImage();
+
+	virtual void setSelectedImage(CCNode* selectedSprite);
+	virtual CCNode* getSelectedImage();
+
+	virtual void setDisabledImage(CCNode* disabledSprite);
+	virtual CCNode* getDisabledImage();
+
+protected:
+	CCNode* m_pNormalImage;
+	CCNode* m_pSelectedImage;
+	CCNode* m_pDisabledImage;
 };
 
 NS_CC_END;
--- cocos2dx/menu_nodes/CCMenuItem.cpp
+++ cocos2dx/menu_nodes/CCMenuItem.cpp
@@ -81,4 +81,119 @@ void CCMenuItem::setTarget(CCObject* rec, SEL_MenuHandler selector)
 	m_pfnSelector = selector;
 }
 
+
+//////////////////////////////////////////////////////////////////////////
+// 字符串菜单，具有点击执行自动放大缩小动画的特性
+// TODO...
+CCMenuItemLabel* CCMenuItemLabel::itemWithLabel(CCNode* label)
+{
+	return itemWithLabel(label, NULL, NULL);
+}
+CCMenuItemLabel* CCMenuItemLabel::itemWithLabel(CCNode* label, CCObject* target, SEL_MenuHandler selector)
+{
+	//TODO...
+	return NULL;
+}
+
+CCMenuItemLabel::CCMenuItemLabel()
+	: m_pLabel(NULL)
+{
+	//TODO...
+}
+
+CCMenuItemLabel::~CCMenuItemLabel()
+{
+	//TODO...
+}
+
+bool CCMenuItemLabel::initWithLabel(CCNode* label, CCObject* target, SEL_MenuHandler selector)
+{
+	//TODO...
+	return false;
+}
+
+void CCMenuItemLabel::setString(const char* label)
+{
+	//TODO...
+}
+
+void CCMenuItemLabel::setLabel(CCNode* label)
+{
+	//TODO...
+}
+
+CCNode* CCMenuItemLabel::getLabel()
+{
+	return m_pLabel;
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+// 精灵菜单，可呈现三种状态精灵(正常，选中，非使能)
+// TODO...
+CCMenuItemSprite* CCMenuItemSprite::itemFromNormalSprite(CCNode* normalSprite, CCNode* selectedSprite, CCNode* disabledSprite)
+{
+	return itemFromNormalSprite(normalSprite, selectedSprite, disabledSprite, NULL, NULL);
+}
+
+CCMenuItemSprite* CCMenuItemSprite::itemFromNormalSprite(CCNode* normalSprite, CCNode* selectedSprite, CCObject* target, SEL_MenuHandler selector)
+{
+	return itemFromNormalSprite(normalSprite, selectedSprite, NULL, target, selector);
+}
+
+CCMenuItemSprite* CCMenuItemSprite::itemFromNormalSprite(CCNode* normalSprite, CCNode* selectedSprite, CCNode* disabledSprite, CCObject* target, SEL_MenuHandler selector)
+{
+	//TODO...
+	return NULL;
+}
+
+CCMenuItemSprite::CCMenuItemSprite()
+	: m_pNormalImage(NULL)
+	, m_pSelectedImage(NULL)
+	, m_pDisabledImage(NULL)
+{
+
+}
+
+CCMenuItemSprite::~CCMenuItemSprite()
+{
+
+}
+
+bool CCMenuItemSprite::initFromNormalSprite(CCNode* normalSprite, CCNode* selectedSprite, CCNode* disabledSprite, CCObject* target, SEL_MenuHandler selector)
+{
+	//TODO...
+	return false;
+}
+
+void CCMenuItemSprite::setNormalImage(CCNode* normalSprite)
+{
+	//TODO...
+}
+
+CCNode* CCMenuItemSprite::getNormalImage()
+{
+	return m_pNormalImage;
+}
+
+void CCMenuItemSprite::setSelectedImage(CCNode* selectedSprite)
+{
+	//TODO...
+}
+
+CCNode* CCMenuItemSprite::getSelectedImage()
+{
+	return m_pSelectedImage;
+}
+
+void CCMenuItemSprite::setDisabledImage(CCNode* disabledSprite)
+{
+	//TODO...
+}
+
+CCNode* CCMenuItemSprite::getDisabledImage()
+{
+	return m_pDisabledImage;
+}
+
 NS_CC_END;
