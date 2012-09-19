09a32b5115d566acdb6f7d953b06b0498083c2af
congcong <120910383@qq.com>
2012-09-19 08:41:55 +0000

    #15 创建CCMenu和CCMenuItem CCMenu处理touch触摸消息

--- cocos2dx/include/CCMenu.h
+++ cocos2dx/include/CCMenu.h
@@ -4,6 +4,17 @@
 
 NS_CC_BEGIN;
 
+typedef enum
+{
+	kCCMenuStateWaiting,
+	kCCMenuStateTrackingTouch,
+} tCCMenuState;
+
+enum
+{
+	kCCMenuTouchPriority = -128,
+};
+
 class CCMenuItem;
 class CC_DLL CCMenu : public CCLayer
 {
@@ -16,8 +27,22 @@ public:
 	bool init();
 	bool initWithItem(CCMenuItem* item);
 
+	virtual void addChild(CCNode* child);
+	virtual void addChild(CCNode* child, int zOrder);
+	virtual void addChild(CCNode* child, int zOrder, int tag);
+	virtual void registerWithTouchDispatcher();
+
 	virtual bool ccTouchBegan(CCTouch* pTouch, CCEvent* pEvent);
 	virtual void ccTouchEnded(CCTouch* pTouch, CCEvent* pEvent);
+
+	virtual void onExit();
+
+protected:
+	CCMenuItem* itemForTouch(CCTouch* touch);
+
+protected:
+	CCMenuItem* m_pSelectedItem;
+	tCCMenuState m_eState;
 };
 
 NS_CC_END;
--- cocos2dx/menu_nodes/CCMenu.cpp
+++ cocos2dx/menu_nodes/CCMenu.cpp
@@ -2,6 +2,7 @@
 #include "CCMenuItem.h"
 #include "CCDirector.h"
 #include "CCPointExtension.h"
+#include "CCTouchDispatcher.h"
 
 NS_CC_BEGIN;
 
@@ -19,6 +20,7 @@ CCMenu* CCMenu::menuWithItem(CCMenuItem* item)
 }
 
 CCMenu::CCMenu()
+	: m_pSelectedItem(NULL)
 {
 
 }
@@ -35,10 +37,13 @@ bool CCMenu::init()
 		setIsTouchEnabled(true);
 
 		CCSize s = CCDirector::sharedDirector()->getWinSize();
-		this->m_bIsRelativeAnchorPoint = false;
+		m_bIsRelativeAnchorPoint = false;
 		setAnchorPoint(ccp(0.5f, 0.5f));
 		setContentSize(s);
 		setPosition(ccp(s.width / 2, s.height / 2));
+
+		m_pSelectedItem = NULL;
+		m_eState = kCCMenuStateWaiting;
 
 		return true;
 	}
@@ -61,9 +66,34 @@ bool CCMenu::initWithItem(CCMenuItem* item)
 	return false;
 }
 
+void CCMenu::addChild(CCNode* child)
+{
+	// 由于重载了addChild(CCNode *child, int zOrder, int tag)导致一个参数的接口被子类屏蔽
+	// 这里相当于将接口public出来，没有添加额外功能
+	CCLayer::addChild(child);
+}
+
+void CCMenu::addChild(CCNode* child, int zOrder)
+{
+	// 由于重载了addChild(CCNode *child, int zOrder, int tag)导致两个参数的接口被子类屏蔽
+	// 这里相当于将接口public出来，没有添加额外功能
+	CCLayer::addChild(child, zOrder);
+}
+
+void CCMenu::addChild(CCNode *child, int zOrder, int tag)
+{
+	// CCMenu只能接受CCMenuItem作为其child来进行管理
+	CCAssert(NULL != dynamic_cast<CCMenuItem*>(child), "Menu only supports MenuItem objects as children");
+	CCLayer::addChild(child, zOrder, tag);
+}
+
+void CCMenu::registerWithTouchDispatcher()
+{
+	CCTouchDispatcher::sharedDispatcher()->addDelegate(this, kCCMenuTouchPriority, true);
+}
+
 bool CCMenu::ccTouchBegan(CCTouch* pTouch, CCEvent* pEvent)
 {
-	CC_UNUSED_PARAM(pTouch);
 	CC_UNUSED_PARAM(pEvent);
 	// 如果menu不可见或者menu的递归parent有不可见，则不响应触摸事件
 	for (CCNode* c = this; c != NULL; c = c->getParent())
@@ -74,7 +104,14 @@ bool CCMenu::ccTouchBegan(CCTouch* pTouch, CCEvent* pEvent)
 		}
 	}
 
-	// 处理item的响应功能TODO...
+	m_pSelectedItem = itemForTouch(pTouch);
+	if (NULL != m_pSelectedItem)
+	{
+		m_eState = kCCMenuStateTrackingTouch;
+		m_pSelectedItem->selected();
+		return true;
+	}
+
 	return false;
 }
 
@@ -82,7 +119,34 @@ void CCMenu::ccTouchEnded(CCTouch* pTouch, CCEvent* pEvent)
 {
 	CC_UNUSED_PARAM(pTouch);
 	CC_UNUSED_PARAM(pEvent);
-	// 处理item的响应功能TODO...
+	CCAssert(kCCMenuStateTrackingTouch == m_eState, "[Menu ccTouchEnded] -- invalid state");	//内部状态错误
+	if (NULL != m_pSelectedItem)
+	{
+		m_pSelectedItem->unselected();
+		// 通知外界按钮激活事件TODO...
+	}
+	m_eState = kCCMenuStateWaiting;
+}
+
+void CCMenu::onExit()
+{
+	if (kCCMenuStateTrackingTouch == m_eState)
+	{
+		if (NULL != m_pSelectedItem)
+			m_pSelectedItem->unselected();
+		m_eState = kCCMenuStateWaiting;
+		m_pSelectedItem = NULL;
+	}
+
+	CCLayer::onExit();
+}
+
+CCMenuItem* CCMenu::itemForTouch(CCTouch* touch)
+{
+	CC_UNUSED_PARAM(touch);
+	// 根据触摸位置来判断当前哪个item被激活
+	// TODO...
+	return NULL;
 }
 
 NS_CC_END;
