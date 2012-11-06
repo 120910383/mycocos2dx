77688d2b34b7a2ab31906395c7f9f052f27b4fcb
congcong <120910383@qq.com>
2012-11-06 02:34:27 +0000

    #18 跨平台处理 添加平台宏，暂时先定义WIN32和LINUX

---
+++ cocos2dx/platform/CCPlatformConfig.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#define CC_PLATFORM_UNKNOWN 0
+#define CC_PLATFORM_WIN32 1
+#define CC_PLATFORM_LINUX 2
+
+#define CC_TARGET_PLATFORM CC_PLATFORM_UNKNOWN
+
+#if !CC_TARGET_PLATFORM && (defined(WIN32) && defined(_WINDOWS))
+#undef CC_TARGET_PLATFORM
+#define CC_TARGET_PLATFORM CC_PLATFORM_WIN32
+#endif
+
+#if !CC_TARGET_PLATFORM && defined(LINUX)
+#undef CC_TARGET_PLATFORM
+#define CC_TARGET_PLATFORM CC_PLATFORM_LINUX
+#endif
+
+#if !CC_TARGET_PLATFORM
+#error "Can not recognize the target platform, compling under a unsupported platform?"
+#endif
