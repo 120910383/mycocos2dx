6793dab0d1d46149b7957d81ddc9205077b8806d
congcong <120910383@qq.com>
2012-11-17 13:01:25 +0000

    #17 整理tests工程测试代码，并去除cocos2dx库中的临时测试代码 完善精灵(Sprite)的测试内容

--- tests/tests/SpriteTest/SpriteTest.cpp
+++ tests/tests/SpriteTest/SpriteTest.cpp
@@ -23,7 +23,37 @@ bool SpriteOpacityTest::init()
 	{
 		CC_BREAK_IF( !TestBasicLayer::init() );
 
-		// TODO...
+		CCSize win_size = CCDirector::sharedDirector()->getWinSize();
+
+		CCSprite* sprite1 = CCSprite::spriteWithFile("Images/man_ini.png");
+		CC_BREAK_IF( NULL == sprite1);
+		sprite1->setPosition(ccp(win_size.width / 2 - 200, win_size.height / 2));
+		sprite1->setOpacity(255);
+		addChild(sprite1);
+
+		CCSprite* sprite2 = CCSprite::spriteWithFile("Images/man_ini.png");
+		CC_BREAK_IF( NULL == sprite2);
+		sprite2->setPosition(ccp(win_size.width / 2 - 100, win_size.height / 2));
+		sprite2->setOpacity(200);
+		addChild(sprite2);
+
+		CCSprite* sprite3 = CCSprite::spriteWithFile("Images/man_ini.png");
+		CC_BREAK_IF( NULL == sprite3);
+		sprite3->setPosition(ccp(win_size.width / 2, win_size.height / 2));
+		sprite3->setOpacity(150);
+		addChild(sprite3);
+
+		CCSprite* sprite4 = CCSprite::spriteWithFile("Images/man_ini.png");
+		CC_BREAK_IF( NULL == sprite4);
+		sprite4->setPosition(ccp(win_size.width / 2 + 100, win_size.height / 2));
+		sprite4->setOpacity(100);
+		addChild(sprite4);
+
+		CCSprite* sprite5 = CCSprite::spriteWithFile("Images/man_ini.png");
+		CC_BREAK_IF( NULL == sprite5);
+		sprite5->setPosition(ccp(win_size.width / 2 + 200, win_size.height / 2));
+		sprite5->setOpacity(50);
+		addChild(sprite5);
 
 		result = true;
 	} while (0);
@@ -54,7 +84,37 @@ bool SpriteColorTest::init()
 	{
 		CC_BREAK_IF( !TestBasicLayer::init() );
 
-		// TODO...
+		CCSize win_size = CCDirector::sharedDirector()->getWinSize();
+
+		CCSprite* sprite1 = CCSprite::spriteWithFile("Images/man_ini.png");
+		CC_BREAK_IF( NULL == sprite1);
+		sprite1->setPosition(ccp(win_size.width / 2 - 200, win_size.height / 2));
+		sprite1->setColor(ccRED);
+		addChild(sprite1);
+
+		CCSprite* sprite2 = CCSprite::spriteWithFile("Images/man_ini.png");
+		CC_BREAK_IF( NULL == sprite2);
+		sprite2->setPosition(ccp(win_size.width / 2 - 100, win_size.height / 2));
+		sprite2->setColor(ccGREEN);
+		addChild(sprite2);
+
+		CCSprite* sprite3 = CCSprite::spriteWithFile("Images/man_ini.png");
+		CC_BREAK_IF( NULL == sprite3);
+		sprite3->setPosition(ccp(win_size.width / 2, win_size.height / 2));
+		sprite3->setColor(ccBLUE);
+		addChild(sprite3);
+
+		CCSprite* sprite4 = CCSprite::spriteWithFile("Images/man_ini.png");
+		CC_BREAK_IF( NULL == sprite4);
+		sprite4->setPosition(ccp(win_size.width / 2 + 100, win_size.height / 2));
+		sprite4->setColor(ccc3(255, 255, 0));
+		addChild(sprite4);
+
+		CCSprite* sprite5 = CCSprite::spriteWithFile("Images/man_ini.png");
+		CC_BREAK_IF( NULL == sprite5);
+		sprite5->setPosition(ccp(win_size.width / 2 + 200, win_size.height / 2));
+		sprite5->setColor(ccc3(0, 255, 255));
+		addChild(sprite5);
 
 		result = true;
 	} while (0);
